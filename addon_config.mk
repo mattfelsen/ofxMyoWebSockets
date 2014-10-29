27c266555a2f2ec75158918352da159c0f15ea95
Matt Felsen <mattfelsen@gmail.com>
2014-10-29 10:57:29 -0400

    Added addon_config.mk with dependencies, closes #1

---
+++ addon_config.mk
@@ -0,0 +1,11 @@
+meta:
+	ADDON_NAME = ofxMyoWebSockets
+	ADDON_DESCRIPTION = openframeworks addon for Thalmic Myo Armbands over the native WebSocket interface
+	ADDON_AUTHOR = Matt Felsen
+	ADDON_TAGS = "myo" "thalmic" "armband" "gesture" "websocket"
+	ADDON_URL = https://github.com/mattfelsen/ofxMyoWebSockets
+
+common:
+	# dependencies with other addons, a list of them separated by spaces 
+	# or use += in several lines
+	ADDON_DEPENDENCIES = ofxJSON ofxLibwebsockets
